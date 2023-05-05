3c991d5606023fd283a0219d4bd6f3e02f38f929
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2023-05-04 22:18:29 -0500

    Initial commit

---
+++ API/Controllers/TasksController.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TasksController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems([FromQuery] string day)
+        {
+            var taskItems = _context.TaskItems.AsQueryable();
+
+            if (day != null) taskItems = taskItems.Where(task => task.Day == day.ToLower());
+
+            return Ok(await taskItems.ToListAsync());
+        }
+
+        [HttpDelete("{taskItemId}")]
+        public async Task<ActionResult> DeleteTaskItem(string taskItemId)
+        {
+            var taskItem = await _context.TaskItems.FirstOrDefaultAsync(task => task.Id.ToString() == taskItemId);
+            if (taskItem == null) return NotFound();
+            _context.TaskItems.Remove(taskItem);
+            if (await _context.SaveChangesAsync() > 0) return NoContent();
+            else return BadRequest();
+        }
+    }
+}
---
+++ API/Entities/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Day { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+    }
+}
